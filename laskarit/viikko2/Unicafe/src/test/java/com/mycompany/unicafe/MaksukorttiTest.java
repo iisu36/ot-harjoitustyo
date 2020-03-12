56d9c777fde107e18a3c7e8e4298a22491361776
Iisakki Kulhua <iisakki@kulhua.net>
2020-03-12 21:02:57 +0200

    Unicafe testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,39 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinKonstruktoriToimiiOikein() {
+        assertEquals("saldo: 10.0",kortti.toString());
+    }
+    
+    @Test
+    public void rahanLataaminenToimiiOikein() {
+        kortti.lataaRahaa(100);
+        assertEquals("saldo: 11.0",kortti.toString());
+    }
+    
+    @Test
+    public void saldonVaheneminenToimiiOikein() {
+        kortti.otaRahaa(200);
+        assertEquals("saldo: 8.0",kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiVäheneJosSeEiRiitä() {
+        kortti.otaRahaa(1100);
+        assertEquals("saldo: 10.0",kortti.toString());
+    }
+    
+    @Test
+    public void tasarahaToimii() {
+        assertTrue(kortti.otaRahaa(1000));
     }
 }
