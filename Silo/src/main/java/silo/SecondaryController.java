d82a89a153643b4d27eecc5153726039ee61010c
Iisakki Kulhua <iisakki@kulhua.net>
2020-03-20 22:08:29 +0200

    käyttölittymän luonnostelua ja testausta

--- Silo/src/main/java/silo/SecondaryController.java
+++ Silo/src/main/java/silo/SecondaryController.java
@@ -1,12 +1,17 @@
 package silo;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private Text mouseOn;
+    
+    @FXML
+    protected void mouseOn(ActionEvent event) throws IOException {
+        mouseOn.toFront();
     }
 }
